cca63b82e4b13e07b4cc77f7cc347d4604214806
IraRadcenko <goncharovaira@ukr.net>
2023-10-26 15:16:04 +0300

    apDate HW32

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-//const chat = document.getElementById('root');
-
 const chatContent = document.getElementById('chatContent');
 
 const chatInput = document.getElementById('chatInput');
@@ -12,58 +10,49 @@ input.appendTo(chatInput);
 const sendBtn = new HTMLElement('button', {type: 'button', id: 'btn'});
 sendBtn.setText('SEND');
 sendBtn.appendTo(chatInput);
-sendBtn.onClick(showUserMessage);
+sendBtn.onClick(displayUserMessage);
 
-const endDialogue = new HTMLElement('div', {id: 'endDialogue'});
-endDialogue.setText('Bot: HI!!!');
-endDialogue.appendTo(chatContent);
-const imgMessage = new HTMLElement('img', {src: 'img/smile3.png'});
-imgMessage.appendTo(chatContent);
-function showUserMessage() {
+const response = responses[Math.floor(Math.random() * responses.length)];
+const responseWaiting = Math.floor(Math.random() * 10 + 1) * 1000;
 
+const responseEnd = Math.floor(Math.random() * 70 + 1) * 1000;
+
+const endDialogueBotMessage = 'Ймовірно, ви не готові до діалогу. До побачення!!!';
+
+const botMessage = 'Я втомився, вибачте!';
+
+const hiMessage = 'Привіт!';
+
+showBotMessage(hiMessage);
+showImg('img/smile3.png');
+
+function displayUserMessage() {
     const userMessageText = document.getElementById('input').value;
+
+   showUserMessage(userMessageText);
+
+    document.getElementById('input').value = '';
 
     if(userMessageText === 'STOP') {
 
-        const userMessage = new HTMLElement('div', {id: 'userMessage'});
-        userMessage.setText(`You: ${userMessageText}`);
-        userMessage.appendTo(chatContent);
+        showBotMessage(endDialogueBotMessage);
+        showImg('img/smile1.png');
+
         document.getElementById('btn').disabled = true;
 
-        const endDialogue = new HTMLElement('div', {id: 'endDialogue'});
-        endDialogue.setText('Bot: You are probably not ready for dialogue. Goodbye!!!');
-        endDialogue.appendTo(chatContent);
-
-        const imgMessage = new HTMLElement('img', {src: 'img/smile1.png'});
-        imgMessage.appendTo(chatContent);
-
-        document.getElementById('input').value = '';
     }else{
-        const userMessage = new HTMLElement('div', {id: 'userMessage'});
-        userMessage.setText(`You: ${userMessageText}`);
-        userMessage.appendTo(chatContent);
-        document.getElementById('input').value = '';
-
-        const response = responses[Math.floor(Math.random() * responses.length)];
-        const responseWaiting = Math.floor(Math.random() * 10 + 1) * 1000;
-
-        function showBotMessage() {
-            const botMessage = new HTMLElement('div', {id: 'botMessage'});
-            botMessage.setText(`Bot: ${response}`);
-            botMessage.appendTo(chatContent);
-        }
-        setTimeout(showBotMessage, responseWaiting);
+        setTimeout(() => showBotMessage(response), responseWaiting);
     }
 }
 
-const responseEnd = Math.floor(Math.random() * 50 + 1) * 1000;
 function showEndBotMessage() {
-    const botMessage = new HTMLElement('div', {id: 'botMessage'});
-    botMessage.setText(`Bot: Я втомився, вибач!`);
-    botMessage.appendTo(chatContent);
+    showBotMessage(botMessage);
 
-    const imgMessage = new HTMLElement('img', {src: 'img/smile.png'});
-    imgMessage.appendTo(chatContent);
+    showImg('img/smile.png');
+
     document.getElementById('btn').disabled = true;
 }
+
 setTimeout(showEndBotMessage, responseEnd);
+
+
